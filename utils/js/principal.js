7f4598710934178dfce1e7f042aeee8a024d5b06
yuri.teixeira <yuri.c.teixeira@hotmail.com>
2018-06-06 13:50:52 -0300

    Incremento de animações.
    Incremento de animações.

--- utils/js/principal.js
+++ utils/js/principal.js
@@ -1,24 +1,75 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $("#motionE1").hide();
     $("#motionN1").hide();
     $("#motionC1").hide();
     $("#motionO1").hide();
+    $("#motionI1").hide();
+    $("#motionN2").hide();
+    $("#motionF1").hide();
+    $("#motionO2").hide();
 
     $("#circleE1").hide();
     $("#circleN1").hide();
     $("#circleC1").hide();
     $("#circleO1").hide();
+    $("#circleI1").hide();
+    $("#circleN2").hide();
+    $("#circleF1").hide();
+    $("#circleO2").hide();
+    $("image").hide();
 
     setTimeout(function () {
         $("#motionE1").show();
-        $("#motionN1").show();
-        $("#motionC1").show();
-        $("#motionO1").show();
-
         $("#circleE1").show();
+    }, 3000);
+    setTimeout(function () {
+        $("#motionN1").show();
         $("#circleN1").show();
-        $("#circleC1").show();
-        $("#circleO1").show();
     }, 6000);
+    setTimeout(function () {
+        $("#motionC1").show();
+        $("#circleC1").show();
+    }, 9000);
+    setTimeout(function () {
+        $("#motionO1").show();
+        $("#circleO1").show();
+    }, 12000);
+    setTimeout(function () {
+        $("#motionI1").show();
+        $("#circleI1").show();
+    }, 15000);
+    setTimeout(function () {
+        $("#motionN2").show();
+        $("#circleN2").show();
+    }, 18000);
+    setTimeout(function () {
+        $("#motionF1").show();
+        $("#circleF1").show();
+    }, 21000);
+    setTimeout(function () {
+        $("#motionO2").show();
+        $("#circleO2").show();
+    }, 24000);
+    setTimeout(function () {
+        $("#motionE1").hide();
+        $("#motionN1").hide();
+        $("#motionC1").hide();
+        $("#motionO1").hide();
+        $("#motionI1").hide();
+        $("#motionN2").hide();
+        $("#motionF1").hide();
+        $("#motionO2").hide();
+
+        $("#circleE1").hide();
+        $("#circleN1").hide();
+        $("#circleC1").hide();
+        $("#circleO1").hide();
+        $("#circleI1").hide();
+        $("#circleN2").hide();
+        $("#circleF1").hide();
+        $("#circleO2").hide();
+
+        $("image").show();
+    }, 29000);
 });
